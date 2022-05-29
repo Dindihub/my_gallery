91dcba9791ec59220aec8bb864ba43960717b941
Dindihub <sadindi03@gmail.com>
2022-05-29 10:43:32 +0300

    Code-Add views for app

--- gallery/views.py
+++ gallery/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
-from .models import Project
+from .models import Photo
 from django.http import Http404
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
-# def photo(request):
-#     try:
-#         photo = Photo.objects.all()
-#     except Photo.DoesNotExist:
-#         raise Http404()
-#     return render(request,'photo.html',{"photos":photo})
+def photo(request):
+    try:
+        photo = Photo.objects.all()
+    except Photo.DoesNotExist:
+        raise Http404()
+    return render(request,'photo.html',{"photos":photo})
+
+
+
+
+def photo_search(request):
+
+    if 'photo' in request.GET and request.GET["photo"]:
+        search_term = request.GET.get("photo")
+        searched_photos = Photo.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"photos": searched_photos})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
     

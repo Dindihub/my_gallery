b8319c1ffef4317038158e4840a59dad1f7e891d
Dindihub <sadindi03@gmail.com>
2022-05-29 10:40:16 +0300

    Code-Add models to admin panel

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import User,Photo,Category
 
 # Register your models here.
+
+class PhotoAdmin(admin.ModelAdmin):
+    filter_horizontal =('photo',)
+
+admin.site.register(User)
+admin.site.register(Photo)
+admin.site.register(Category)
+
+
